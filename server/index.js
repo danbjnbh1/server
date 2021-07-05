249359c2a80db18ee47c979df301c7f58780a7e6
dan nachmany <danbjnbh1@gmail.com>
2021-07-05 17:16:27 +0300

    1

--- server/index.js
+++ server/index.js
@@ -10,6 +10,17 @@ mongoose.connect(mongoURL, {
   useFindAndModify: false,
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
